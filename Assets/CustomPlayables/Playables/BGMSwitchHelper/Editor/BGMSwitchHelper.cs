d52719cbf2b8ea4d09b86ef42425dec0cb10bab1
shenzhixiong_313322 <bilter1001@163.com>
2020-06-14 23:23:17 +0800

    提交原始代码，包括读取json文件，创建时间轴文件.playable,创建音频轨道和创建音频clip

---
+++ Assets/CustomPlayables/Playables/BGMSwitchHelper/Editor/BGMSwitchHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace BGMSwitchHelper
+{
+    class BGMSwitchHelper : EditorWindow
+    {
+
+        [Serializable]
+        public class BGMConfig
+        {
+            [Serializable]
+            public class Item
+            {               
+                public string id;
+                public string name;
+            }
+
+            public class Topic
+            {
+                public string topicname;
+                public List<Item> items;
+            }
+            public List<Topic> topics = new List<Topic>();
+        }
+
+        protected static string configFile = "Assets/StreamingAssets/Config/BGMConfig.json";
+        public static BGMConfig config = new BGMConfig();
+
+        public static PlayableDirector bgmPlayableDirector;
+        public static GameObject BMGMgrGo;
+        static string playableDir = "Assets/BGMPlayable/";
+        static string playableName = "BgmSetting.playable";
+
+        [MenuItem("自动配乐/设置")]
+        static void CreateVLGameObject()
+        {
+            BMGMgrGo = GameObject.Find("BMGMgrGo");
+            if (BMGMgrGo == null)
+            {
+                BMGMgrGo = new GameObject("BMGMgrGo");
+                bgmPlayableDirector = BMGMgrGo.AddComponent<PlayableDirector>();
+                BMGMgrGo.AddComponent<AudioSource>();
+            }
+            else
+            {
+                bgmPlayableDirector = BMGMgrGo.GetComponent<PlayableDirector>();
+            }
+            if (!Directory.Exists(playableDir))
+            {
+                Directory.CreateDirectory(playableDir);
+            }
+            string playablePath = playableDir + playableName;
+
+            if (!File.Exists(playablePath))
+            {
+                var asset = TimelineAsset.CreateInstance<TimelineAsset>();
+                AssetDatabase.CreateAsset(asset, playablePath);
+                var track = asset.CreateTrack<AudioTrack>(null, "BgmTrack");
+                AssetDatabase.SaveAssets();
+                bgmPlayableDirector.playableAsset = asset;
+            }
+            else
+            {
+                TimelineAsset asset = AssetDatabase.LoadAssetAtPath<TimelineAsset>(playablePath);
+                bgmPlayableDirector.playableAsset = asset;
+            }
+
+
+
+            string json = File.ReadAllText(configFile);
+            config = JsonConvert.DeserializeObject<BGMConfig>(json);
+
+            BMGSwitchWindow _window = EditorWindow.GetWindow<BMGSwitchWindow>(); // 创建自定义窗体
+            _window.titleContent = new GUIContent("自动配乐"); // 窗口的标题
+            _window.minSize = new Vector2(250, 400);
+            _window.Show();
+        }
+
+
+
+
+    }
+}
---
+++ Assets/CustomPlayables/Playables/BGMSwitchHelper/Editor/BMGSwitchWindow.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace BGMSwitchHelper
+{
+    class BMGSwitchWindow : EditorWindow
+    {
+
+
+        private void OnGUI()
+        {
+            GUILayout.BeginVertical();
+            //
+            GUILayout.BeginHorizontal();
+            //区域B 展示区域
+            int x = 0;
+            int y = 0;
+            int size = 60;
+            int padding = 10;
+            int maxCount = 600 / (size + padding);
+            int count = 0;
+            Rect rect = Rect.zero;
+            Rect labelRect = Rect.zero;
+            GUILayout.BeginArea(new Rect(padding, padding, 600, 600));
+            GUILayout.BeginScrollView(new Vector2(300, 0),
+                GUILayout.Width(600), GUILayout.ExpandHeight(true));
+
+            var nodeInfos = BGMSwitchHelper.config.topics;
+            if (nodeInfos != null)
+            {
+                foreach (var nodeInfo in nodeInfos)
+                {
+                    if (nodeInfo == null)
+                    {
+                        return;
+                    }
+                    rect = new Rect(x, y, size, size);
+                    string topicname = nodeInfo.topicname;
+                    if (GUI.Button(rect, topicname))
+                    {
+                        System.Random random = new System.Random();
+                        int rat = random.Next(nodeInfo.items.Count);
+                        Debug.Log("rat: " + rat);
+                        string id = nodeInfo.items[rat].id;
+                        AudioClip audioclip = Resources.Load<AudioClip>(id);
+
+                        if (audioclip == null)
+                        {
+                            Debug.LogError("audioclip is null. path: " + id);
+                        }
+                        else
+                        {
+                            TimelineAsset timelineAsset = BGMSwitchHelper.bgmPlayableDirector.playableAsset as TimelineAsset;
+                            List<TrackAsset> lstTrackAsset = timelineAsset.GetRootTracks().ToList<TrackAsset>();
+                            for (int i = 0; i < lstTrackAsset.Count; i++)
+                            {
+                                TrackAsset trackAsset = timelineAsset.GetRootTrack(i);
+                                timelineAsset.DeleteTrack(trackAsset);
+                            }
+                            var track = timelineAsset.CreateTrack<AudioTrack>(null, "bgmTrack");
+                            BGMSwitchHelper.bgmPlayableDirector.SetGenericBinding(track, BGMSwitchHelper.BMGMgrGo);
+
+
+                            var clip = track.CreateClip(audioclip);
+                            clip.displayName = audioclip.name;
+                            AudioPlayableAsset clipasset = clip.asset as AudioPlayableAsset;
+                            clipasset.loop = true;
+                            AssetDatabase.SaveAssets();
+                        }  
+
+                        Close();
+                    }
+
+                    x += size + padding;
+                    count++;
+                    if ((count + 1) % maxCount == 0)
+                    {
+                        x = 0;
+                        y += size + padding;
+                    }
+                }
+            }
+
+            GUILayout.EndScrollView();
+            GUILayout.EndArea();
+            GUILayout.EndHorizontal();
+
+//             if (GUILayout.Button("Generate"))
+//             {
+// 
+//             }
+
+
+            GUILayout.EndVertical();
+        }
+
+    }
+}
